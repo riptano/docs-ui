af3ca2802cf72ba6babc7ac645348ed577db7467
Ronnie Miller <ronnie.miller@datastax.com>
2023-09-28 11:07:46 -0700

    Modify svg helper so it references path correctly in preview and build (#17)

--- src/helpers/svg.js
+++ src/helpers/svg.js
@@ -1,10 +1,9 @@
 'use strict'
 
-const fs = require('fs')
 const path = require('path')
 
-module.exports = (iconName) => {
-  const pathName = path.join(__dirname, '/src/img/', iconName + '.svg')
-  const content = fs.readFileSync(pathName, 'utf8')
-  return content
+module.exports = (iconName, { data }) => {
+  const uiRootPath = data.root.uiRootPath
+  const svgFilePath = path.join(uiRootPath, 'img', iconName + '.svg')
+  return '<img src="' + svgFilePath + '" alt="' + iconName + '" />'
 }
