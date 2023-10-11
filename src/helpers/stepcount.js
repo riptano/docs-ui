2b4e60353005b97fc5ac8353374b2d705f0846f4
Cole Goldsmith <cole.b.goldsmith@gmail.com>
2023-10-11 15:16:16 -0500

    push the step changes (#28)

--- src/helpers/stepcount.js
+++ src/helpers/stepcount.js
@@ -1,7 +1,7 @@
 'use strict'
 
 module.exports = (html) => {
-  const matches = html.toString().match(/class="step"/g)
+  const matches = html.toString().match(/class(?=="((.*?\s)?step(\s.*?)?)")/g)
   if (!matches) return undefined
   return matches.length
 }
