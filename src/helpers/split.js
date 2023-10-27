ce38a7c4f00cb80610bb6521d0bf3f5793942828
Josué Bran <josuebran@gmail.com>
2023-10-27 12:13:03 -0600

    fix split helper

--- src/helpers/split.js
+++ src/helpers/split.js
@@ -1,18 +1,10 @@
 'use strict'
 
 module.exports = (val) => {
-  if(typeof val === 'string') {
+  if (typeof val === 'string') {
     const str = val.split(', ')
-    console.log(str)
     return str
   } else {
-    return 'no string :('
+    throw new Error('{{split}} helper expects a string argument')
   }
-  //return typeof val
-  //if (typeof val === 'string') {
-    //const array = val.split(',')
-    //return 'yes its a string'
-  //} else {
-  //  throw new Error('{{split}} helper expects a string argument')
-  //}
 }
