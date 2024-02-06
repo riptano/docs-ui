bd74deb8a82b08e8aba5b77d54b3080d06f3a43f
Cole Goldsmith <cole.goldsmith@datastax.com>
2024-02-06 13:21:29 -0600

    add js for scrolling buttons

---
+++ src/js/08-tab-scroll-buttons.js
@@ -0,0 +1,42 @@
+;(function () {
+  'use strict'
+
+  document.querySelectorAll('.tabs').forEach((block) => {
+    var container = block.querySelector('.tablist')
+    var ulist = block.querySelector('.tablist > ul')
+
+    // Create scroll buttons
+    const scrollRightButton = document.createElement('button')
+    scrollRightButton.className = 'material-icons btn btn-neutral btn-plain w-2 invisible opacity-0'
+    scrollRightButton.textContent = 'chevron_right'
+    container.append(scrollRightButton)
+
+    const scrollLeftButton = document.createElement('button')
+    scrollLeftButton.className = 'material-icons btn btn-neutral btn-plain w-2 invisible opacity-0'
+    scrollLeftButton.textContent = 'chevron_left'
+    container.prepend(scrollLeftButton)
+
+    // Function to check scroll and show/hide buttons
+    const checkScroll = () => {
+      scrollLeftButton.classList.toggle('invisible', ulist.scrollLeft === 0)
+      scrollLeftButton.classList.toggle('opacity-0', ulist.scrollLeft === 0)
+      scrollRightButton.classList.toggle('invisible', ulist.scrollWidth <= (ulist.clientWidth + ulist.scrollLeft + 1))
+      scrollRightButton.classList.toggle('opacity-0', ulist.scrollWidth <= (ulist.clientWidth + ulist.scrollLeft + 1))
+    }
+
+    // Check scroll initially
+    checkScroll()
+
+    // Check scroll whenever the block is scrolled
+    ulist.addEventListener('scroll', checkScroll)
+
+    // Scroll when buttons are clicked
+    scrollLeftButton.addEventListener('click', () => {
+      ulist.scrollLeft -= 100
+    })
+
+    scrollRightButton.addEventListener('click', () => {
+      ulist.scrollLeft += 100
+    })
+  })
+})()
