514b0b3198f4bd62c3eb835aba3bd43ba2166972
Cole Goldsmith <cole.goldsmith@datastax.com>
2024-03-06 14:40:05 -0600

    use background body for backdrop

--- src/js/vendor/zooming.bundle.js
+++ src/js/vendor/zooming.bundle.js
@@ -5,7 +5,12 @@
 
   const prefersReducedMotion = window.matchMedia('(prefers-reduced-motion: reduce)').matches
 
-  const zooming = new Zooming({ bgColor: 'rgb(10, 10, 10)', bgOpacity: 0.6, scaleBase: 0.8, transitionDuration: prefersReducedMotion ? 0.01 : 0.3 })
+  const zooming = new Zooming({
+    bgColor: 'var(--ds-background-body)',
+    bgOpacity: 0.9,
+    scaleBase: 0.8,
+    transitionDuration: prefersReducedMotion ? 0.01 : 0.3,
+  })
 
   zooming.listen('.doc .imageblock img, .doc .image img')
 })()
