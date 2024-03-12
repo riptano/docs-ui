07ccace995d445bd2b4d94434a2b8898ef653f5d
Cole Goldsmith <cole.b.goldsmith@gmail.com>
2024-03-12 10:46:04 -0500

    Use Zooming to enlarge images on click (#106)
    * add lightbox code from old ui, tweak css a little
    
    * use the zooming lib instead, its better
    
    * use background body for backdrop

---
+++ src/js/vendor/zooming.bundle.js
@@ -0,0 +1,16 @@
+;(function () {
+  'use strict'
+
+  const Zooming = require('zooming')
+
+  const prefersReducedMotion = window.matchMedia('(prefers-reduced-motion: reduce)').matches
+
+  const zooming = new Zooming({
+    bgColor: 'var(--ds-background-body)',
+    bgOpacity: 0.9,
+    scaleBase: 0.8,
+    transitionDuration: prefersReducedMotion ? 0.01 : 0.3,
+  })
+
+  zooming.listen('.doc .imageblock img, .doc .image img')
+})()
