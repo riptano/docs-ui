a6929e3a64c9cbfbddcceafa1eebea5db34b27b3
Cole Goldsmith <cole.goldsmith@datastax.com>
2024-05-16 16:46:34 -0500

    global nav ignored component urls

--- src/helpers/global-nav-active.js
+++ src/helpers/global-nav-active.js
@@ -2,27 +2,45 @@
 
 module.exports = module.exports = (navItem, {
   data: {
-    root: { page },
+    root: { page, site },
   },
 }) => {
   const pageVersion = page.componentVersion?.version
   const pageComponent = page.component?.name
 
-  const matchesComponentAndVersion = (item) => {
-    if (pageVersion) {
-      return item.component === pageComponent && item.version === pageVersion
-    } else {
-      return item.component === pageComponent
-    }
+  const getIgnoredActiveComponents = (items) => {
+    return items.reduce((acc, item) => {
+      if (item.ignoreActiveComponent) {
+        acc[item.url] = item.component
+      }
+      if (item.items) {
+        acc = { ...acc, ...getIgnoredActiveComponents(item.items) }
+      }
+      return acc
+    }, {})
   }
 
-  if (navItem.component) {
-    return matchesComponentAndVersion(navItem)
+  const ignoredActiveComponents = getIgnoredActiveComponents(site.keys.globalNav)
+
+  const matchesComponentAndVersion = (item) => {
+    if (ignoredActiveComponents[page.url] === pageComponent && !item.ignoreActiveComponent) {
+      return false
+    }
+    if (item.ignoreActiveComponent) {
+      return item.url === page.url
+    }
+    if (item.component) {
+      if (item.version && pageVersion) {
+        return item.component === pageComponent && item.version === pageVersion
+      }
+      return item.component === pageComponent
+    }
+    return false
   }
 
   if (navItem.items) {
     return navItem.items.some(matchesComponentAndVersion)
+  } else {
+    return matchesComponentAndVersion(navItem)
   }
-
-  return false
 }
