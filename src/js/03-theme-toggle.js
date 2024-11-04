9ccd250d7402229c92fec9ef2b17828b72a09d5b
Cole Goldsmith <cole.b.goldsmith@gmail.com>
2024-11-04 16:06:55 -0600

    theme toggle and more updates

--- src/js/03-theme-toggle.js
+++ src/js/03-theme-toggle.js
@@ -2,22 +2,40 @@
 ;(function () {
   'use strict'
 
-  var toggle = document.getElementById('theme-toggle')
+  var toggleButtons = document.querySelectorAll('#theme-toggle .toggle-button')
+  if (!toggleButtons) return
 
-  var prefersTheme = window.matchMedia('(prefers-color-scheme: dark)').matches ? 'dark' : 'light'
-  var storedTheme = localStorage.getItem('theme') || prefersTheme
+  var storedTheme = localStorage.getItem('themePreference')
+  var theme = storedTheme || 'system'
 
-  if (!toggle) return
+  var toggleThumb = document.querySelector('#theme-toggle .thumb')
+  toggleThumb.classList.add(theme)
 
-  toggle.onclick = function () {
-    var currentTheme = document.documentElement.getAttribute('data-theme') || storedTheme || 'light'
-    var targetTheme = 'light'
-
-    if (currentTheme === 'light') {
-      targetTheme = 'dark'
-    }
-
-    document.documentElement.setAttribute('data-theme', targetTheme)
-    localStorage.setItem('theme', targetTheme)
+  var currentButton = document.querySelector(`#theme-toggle .toggle-button[value="${theme}"]`)
+  if (currentButton) {
+    currentButton.ariaPressed = 'true'
+    currentButton.classList.add('active')
   }
+
+  toggleButtons.forEach((button) => {
+    button.onclick = function () {
+      var systemPreference = window.matchMedia('(prefers-color-scheme: dark)').matches ? 'dark' : 'light'
+      var currentTheme = localStorage.getItem('themePreference') || systemPreference
+      if (button.value === currentTheme) return
+      if (button.value === 'system') {
+        document.documentElement.setAttribute('data-theme', systemPreference)
+      } else {
+        document.documentElement.setAttribute('data-theme', button.value)
+      }
+      localStorage.setItem('themePreference', button.value)
+      toggleThumb.classList.remove('system', 'light', 'dark')
+      toggleThumb.classList.add(button.value)
+      toggleButtons.forEach((btn) => {
+        btn.ariaPressed = 'false'
+        btn.classList.remove('active')
+      })
+      button.ariaPressed = 'true'
+      button.classList.add('active')
+    }
+  })
 })()
