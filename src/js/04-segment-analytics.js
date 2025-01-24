ec609506c296ec09aefefb932cb07eadc53630c2
Cole Goldsmith <cole.b.goldsmith@gmail.com>
2025-01-24 13:49:17 -0600

    update to latest changes from marketing without gtag scripts

--- src/js/04-segment-analytics.js
+++ src/js/04-segment-analytics.js
@@ -32,49 +32,4 @@
   // Expose trackEvent and trackLinkEvent to the global scope.
   window.trackEvent = trackEvent
   window.trackLinkEvent = trackLinkEvent
-
-  // Save the Ketch consent state to the global scope and add consent middleware to analytics calls.
-  if (window && window.ketch && window.analytics) {
-    const saveConsent = (consent) => {
-      window.ketchConsent = consent
-    }
-    window.ketch('on', 'consent', saveConsent)
-    window.ketch('on', 'userConsentUpdated', saveConsent)
-    window.ketch('on', 'regionInfo', (regionInfo) => {
-      const customTextRegions = ['US-CA']
-      if (customTextRegions.includes(regionInfo)) {
-        const preferenceCenterLinkElement = document.getElementById('preferenceCenterLink')
-        preferenceCenterLinkElement.textContent = 'Do Not Sell My Personal Information'
-      }
-    })
-    window.analytics.addSourceMiddleware(({ payload, next }) => {
-      if (window.ketchConsent) {
-        payload.obj.properties = {
-          ...(payload.obj.properties || {}),
-          'Analytics Storage Consent State':
-            (window.ketchConsent?.purposes || {})?.analytics === true
-              ? 'granted'
-              : 'denied',
-          'Ads Storage Consent State':
-            (window.ketchConsent?.purposes || {})?.targeted_advertising === true
-              ? 'granted'
-              : 'denied',
-          'Ad User Data Consent State':
-            (window.ketchConsent?.purposes || {})?.targeted_advertising === true
-              ? 'granted'
-              : 'denied',
-          context: {
-            consent: {
-              categoryPreferences: window.ketchConsent?.purposes || {},
-            },
-          },
-          traits: window.ketchConsent?.purposes || {},
-        }
-        payload.obj.context.consent = {
-          categoryPreferences: window.ketchConsent?.purposes,
-        }
-      }
-      next(payload)
-    })
-  }
 })()
---
+++ src/js/10-ketch-consent.js
@@ -0,0 +1,64 @@
+;(function () {
+  'use strict'
+
+  window.analytics.ready(() => {
+    window.ketch('once', 'consent', onKetchConsent)
+    window.ketch('on', 'userConsentUpdated', onKetchConsentUpdated)
+  })
+
+  // If the user is in the US-CA region, change the text of the preference center link
+  window.ketch('on', 'regionInfo', (regionInfo) => {
+    const customTextRegions = ['US-CA']
+    if (customTextRegions.includes(regionInfo)) {
+      const preferenceCenterLinkElement = document.getElementById('preferenceCenterLink')
+      if (preferenceCenterLinkElement) {
+        preferenceCenterLinkElement.textContent = 'Do Not Sell My Personal Information'
+      }
+    }
+  })
+
+  // If the user is in the default jurisdiction, remove the preference center link
+  window.ketch('on', 'jurisdiction', (jurisdiction) => {
+    if (jurisdiction.includes('default')) {
+      const preferenceCenterContainerElement = document.getElementById('preferenceCenterContainer')
+      if (preferenceCenterContainerElement) {
+        preferenceCenterContainerElement.remove()
+      }
+    }
+  })
+
+  // Once - This will be fired only one time, will initialize all the main features.
+  const onKetchConsent = (consent) => {
+    window.ketchConsent = consent
+    addKetchConsentToContextMiddleware()
+    window.analytics.page('Consent Update')
+    // loadScripts(); // Load any script if we have scripts to fire after ketch consent is fired.
+  }
+
+  // on - Each time the user changes the preferences, save them to the global variable
+  const onKetchConsentUpdated = (consent) => {
+    window.ketchConsent = consent
+  }
+
+  // Use the analytics.addSourceMiddleware function to include the consent on all the events
+  const addKetchConsentToContextMiddleware = () => {
+    window.analytics.addSourceMiddleware(({ payload, next }) => {
+      if (window.ketchConsent) {
+        const analyticsString = window.ketchConsent.purposes.analytics === true ? 'granted' : 'denied'
+        const targetedAdsString = window.ketchConsent.purposes.targeted_advertising === true ? 'granted' : 'denied'
+
+        payload.obj.properties = {
+          ...(payload.obj.properties || {}),
+          analyticsStorageConsentState: analyticsString,
+          adsStorageConsentState: targetedAdsString,
+          adUserDataConsentState: targetedAdsString,
+          adPersonalizationConsentState: targetedAdsString,
+        }
+        payload.obj.context.consent = {
+          categoryPreferences: window.ketchConsent?.purposes,
+        }
+      }
+      next(payload)
+    })
+  }
+})()
