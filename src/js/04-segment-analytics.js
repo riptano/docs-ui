977b474cbb915bc8b2e6219b9d5e131d9c55a34c
Eric Schneider <37347760+eric-schneider@users.noreply.github.com>
2025-01-27 09:41:45 -0800

    DOC-4329: Ketch footer and Segment updates (#179)
    * Add consent management link to footer
    
    * dynamic footer text and consent context added to segment calls
    
    * fix consent object
    
    * Make footer link conditional
    
    * Shorten data-track attribute
    
    * Squash merge remove-gtm-scripts into DOC-4329-footer
    
    * fix track events so they use consent context correctly. Only load segment if the user consents to analytics
    
    * manual ketch url for testing purposes, revert this soon
    
    * test showing ketch preferences on load
    
    * fix segment object
    
    * load segment initially, update ketch scripts
    
    * hard code keys
    
    * undo hard coded site keys
    
    * add ketch logic to js file and let middleware handle context
    
    * update to latest changes from marketing without gtag scripts
    
    * UNDO THIS - hard coding segment and ketch site keys
    
    * use segment prod source
    
    * force a rebuild
    
    * use dev segment source
    
    * add gtag script and use staging www site footer link
    
    * put site keys back
    
    ---------
    
    Co-authored-by: Cole Goldsmith <cole.b.goldsmith@gmail.com>

--- src/js/04-segment-analytics.js
+++ src/js/04-segment-analytics.js
@@ -1,18 +1,35 @@
 ;(function () {
   'use strict'
 
+  const trackEvent = (name, payload) => {
+    if (window.analytics) {
+      window.analytics.track(name, payload || {})
+    }
+  }
+
+  const trackLinkEvent = (element, name, payload) => {
+    if (window.analytics) {
+      window.analytics.trackLink(element, name, payload || {})
+    }
+  }
+
+  // Add click event listeners to all elements with a data-track attribute.
   if (window.analytics) {
     const trackedLinkElements = document.querySelectorAll('a[data-track]')
     const trackedElements = document.querySelectorAll('[data-track]:not(a)')
 
     trackedLinkElements.forEach((element) => {
-      window.analytics.trackLink(element, element.dataset.track)
+      trackLinkEvent(element, element.dataset.track)
     })
 
     trackedElements.forEach((element) => {
       element.addEventListener('click', (e) => {
-        window.analytics.track(element.dataset.track)
+        trackEvent(element.dataset.track)
       })
     })
   }
+
+  // Expose trackEvent and trackLinkEvent to the global scope.
+  window.trackEvent = trackEvent
+  window.trackLinkEvent = trackLinkEvent
 })()
